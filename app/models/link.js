0f9c69c00b710f3a941b43db28a52d375e51c7a8
Valeria Aleinikova <aleinikavalera@gmail.com>
2017-07-17 13:28:33 +0300

    Added process of generating short link

--- app/models/link.js
+++ app/models/link.js
@@ -1,10 +1,45 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-module.exports = mongoose.model('Link', new Schema({
+var Link = new Schema({
   originalLink: String,
   shortLink: String,
   author: String,
   count: {type: Number, default: 0},
   tags: {type: Array, default: []}
-}));
+});
+
+Link.pre('save', function(next) {
+  this.constructor.find({
+    originalLink: this.originalLink,
+  })
+    .then(links => {
+      var shortLinks = links.map(link => link.shortLink);
+
+      var newShortLink = generateShortLink(4);
+
+      while (shortLinks.indexOf(newShortLink) > -1) {
+        newShortLink = generateShortLink(4);
+      }
+
+      this.shortLink = newShortLink;
+
+      next();
+    });
+});
+
+function generateShortLink(n) {
+  var abc ='abcdefghijklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ0123456789';
+  var abcLength = abc.length;
+  var genLink ='lera.com/';
+  var linkLength = n + genLink.length;
+
+  while(genLink.length < linkLength) {
+    var index = Math.floor ( Math.random() * abcLength );
+    genLink += abc[index];
+  }
+
+  return genLink;
+}
+
+module.exports = mongoose.model('Link', Link);
--- app/routes/link.js
+++ app/routes/link.js
@@ -9,7 +9,6 @@ linkRoutes.post('/create', function(req, res) {
 
   var link = new Link({
     originalLink: req.body.link,
-    shortLink: generateLink(2),
     count: req.body.count,
   });
 
@@ -26,52 +25,12 @@ linkRoutes.post('/create', function(req, res) {
         link = Object.assign(link, {author: user})
         link.save(function(err) {
           if (err) throw err;
-
           console.log("Link was created", link);
-          console.log(link)
           res.json({ success: true, message: link});
         })
       }
     })
   }
 });
-
-
-function generateLink(n) {
-  var abc ='ab';//cdefghijklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ',
-  var abcLength = abc.length;
-  var genLink ='lera.com/';
-  var linkLength = n + genLink.length;
-  var isExist = false;
-
-  while(genLink.length < linkLength) {
-    var index = Math.floor ( Math.random() * abcLength );
-    genLink += abc[index];
-  }
-}
-
-function findLink(searchingLink){
-  var isExist;
-  var findLink = new Promise(function(resolve, reject) {
-    Link.findOne({
-    shortLink: searchingLink
-    }, function(err, link) {
-      if (err) reject(err);
-      resolve(link);
-    });
-  });
- return findLink.then(
-    function(result) {
-      if (result) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    function(error) {
-      console.log(error);
-    }
-  );
-}
 
 module.exports = linkRoutes;
